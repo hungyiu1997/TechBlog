a572fb75a8ab194c7a05e735a9c765f5a89fb13c
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-09-23 16:12:25 -0400

    adding to comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,6 +7,9 @@ router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       // TODO: COMMENT BODY IN REQUEST USING SPREAD
+      // use it to break an object apart, and arrays
+      ...req.body,
+      user_id:req.session.user_id,
 
       // TODO: SET USERID userId TO SESSION LOGGEDIN USERID
     });
